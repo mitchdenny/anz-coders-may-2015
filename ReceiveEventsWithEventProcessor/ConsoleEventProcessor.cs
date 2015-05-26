854d0650538a3a91cb65c4899fc413e99516a1b9
Mitch Denny <mitch@mitchdenny.com>
2015-05-26 14:13:31 +1000

    Implemented the event processor host.

---
+++ ReceiveEventsWithEventProcessor/ConsoleEventProcessor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceiveEventsWithEventProcessor
+{
+    public class ConsoleEventProcessor : IEventProcessor
+    {
+        public async Task CloseAsync(PartitionContext context, CloseReason reason)
+        {
+        }
+
+        public async Task OpenAsync(PartitionContext context)
+        {
+        }
+
+        public async Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        {
+            foreach (var message in messages)
+            {
+                var json = Encoding.UTF8.GetString(message.GetBytes());
+                Console.WriteLine(json);
+            }
+
+            await context.CheckpointAsync();
+        }
+    }
+}
--- ReceiveEventsWithEventProcessor/Program.cs
+++ ReceiveEventsWithEventProcessor/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,18 @@ namespace ReceiveEventsWithEventProcessor
     {
         public static void Main(string[] args)
         {
+            var host = new EventProcessorHost(
+                "localhost",
+                "readings",
+                "$Default",
+                "Endpoint=sb://anzcoders.servicebus.windows.net/;SharedAccessKeyName=receive201502260001;SharedAccessKey=ZzsySuzZ7kvxOlgEQZTqFz61aYtdSRdAWNvGJFY6FOg=",
+                "DefaultEndpointsProtocol=https;AccountName=anzcoders;AccountKey=kToCa3fs8IwN+wbawrruY6HSM+T1O3gaRMYg8ki8CmlKzWGs5UVtveo0UsCrO6xQgH2kHzq3dhN/O0C3bEQpUQ=="
+                );
+
+            host.RegisterEventProcessorAsync<ConsoleEventProcessor>().Wait();
+
+            Console.WriteLine("Press ENTER to stop.");
+            Console.ReadLine();
         }
     }
 }
